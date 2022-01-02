c64629af52e89f63d6f226694066e03880396258
kashif mehmood <kashismails@gmail.com>
2022-01-02 03:25:23 -0800

    initial commit

---
+++ app/src/main/java/com/infinity/navigationsample/models/Result.kt
@@ -0,0 +1,22 @@
+package com.infinity.navigationsample.models
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+
+@Parcelize
+
+data class Result(
+
+    val id: String,
+    val backdrop_path: String,
+    val overview: String,
+    val popularity: String,
+    val poster_path: String,
+    val release_date: String,
+    val title: String,
+    val vote_average: String,
+    val popular : Boolean,
+    val latest : Boolean
+) : Parcelable
+
